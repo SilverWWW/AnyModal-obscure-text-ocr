b7cfb6378988bc2cd47f0b9cda31d1efabe8eb58
Will S <wsilver222@gmail.com>
2025-04-07 15:08:32 +0000

    inference

--- obscure-text-ocr/inference.py
+++ obscure-text-ocr/inference.py
@@ -1,3 +1,5 @@
+# pip install huggingface_hub transformers torch schedulefree peft datasets bitsandbytes sentencepiece
+# pip install -U pillow
 import llm
 import anymodal
 import torch
@@ -17,7 +19,7 @@ import matplotlib.pyplot as plt
 # Load language model and tokenizer
 llm_tokenizer, llm_model = llm.get_llm(
     "meta-llama/Llama-3.2-1B", 
-    access_token='GET_YOUR_OWN_TOKEN_FROM_HUGGINGFACE',
+    access_token='filler',
     quantized = False,
     use_peft = False
 )
@@ -29,93 +31,76 @@ llm_hidden_size = llm.get_hidden_size(llm_tokenizer, llm_model)
 llm_model.to(device)
 
 # Dataset configuration
-dataset_name = "unsloth/LaTeX_OCR"
+dataset_name = "MiXaiLL76/TextOCR_OCR"
 
 # Load vision model components
 # image_processor, vision_model, vision_hidden_size = vision.get_image_encoder('google/vit-base-patch16-224', use_peft=False)
 image_processor, vision_model, vision_hidden_size = vision.get_image_encoder('google/siglip-so400m-patch14-384', use_peft=False)
 
-ds = vision.TestDataset(dataset_name, image_processor, name = None, split = 'test')
-
+# Load dataset
+ds = vision.ImageDataset(dataset_name, image_processor, name = None, split = 'test')
 
 # Initialize vision tokenizer and encoder
 vision_encoder = vision.VisionEncoder(vision_model)
 vision_tokenizer = vision.Projector(vision_hidden_size, llm_hidden_size, num_hidden=1)
 
 
-# Initialize MultiModalModel
-multimodal_model = anymodal.MultiModalModel(
+# Initialize Fine Tuned Model
+multimodal_model_trained = anymodal.MultiModalModel(
     input_processor=None,
     input_encoder=vision_encoder,
     input_tokenizer=vision_tokenizer,
     language_tokenizer=llm_tokenizer,
     language_model=llm_model,
-    prompt_text="The latex expression of the equation in the image is:")
+    prompt_text="The text in the image is: ")
 
-if not os.path.exists("latex_ocr"):
-    os.makedirs("latex_ocr")
+multimodal_model_trained._load_model('obscure_text_ocr_again_3')
+multimodal_model_trained.eval()
 
-snapshot_download("AnyModal/latex-ocr-Llama-3.2-1B", local_dir="latex_ocr")
+# Initialize Base Model
+multimodal_model_base = anymodal.MultiModalModel(
+    input_processor=None,
+    input_encoder=vision_encoder,
+    input_tokenizer=vision_tokenizer,
+    language_tokenizer=llm_tokenizer,
+    language_model=llm_model,
+    prompt_text="The text in the image is: ")
 
-multimodal_model._load_model('latex_ocr')
+multimodal_model_base._load_model('obscure_text_ocr_dummy_1')
+multimodal_model_base.eval()
 
-# Generate captions for a few images and plot the images and save captions in txt file
-multimodal_model.eval()
+# Generate captions comparatively with the trained and base model
 
 os.makedirs("temp", exist_ok=True)
 
-for _ in range(5):
-    sample_idx = np.random.randint(len(ds))
+test_samples = 1000
+correct_generations_trained = 0
+correct_generations_base = 0
+
+with open(f"temp/generations.txt", "w") as f:
+    f.write(f"OCR Results Comparison\n")
+    f.write(f"====================\n\n")
+
+for i in range(test_samples):
+    sample_idx = i
     sample = ds[sample_idx]
     
-    # save the image with the caption and the generated caption
-    image = sample['image']
     caption = sample['text']
-    generated_caption = multimodal_model.generate(sample['input'], max_new_tokens=120, do_sample = True, num_beams = 3)
+    generated_caption_1 = multimodal_model_trained.generate(sample['input'], max_new_tokens=120, do_sample = True, num_beams = 3)
+    generated_caption_2 = multimodal_model_base.generate(sample['input'], max_new_tokens=120, do_sample = True, num_beams = 3)
 
-    plt.imshow(image)
-    plt.axis('off')
-    plt.savefig(f"temp/image_{sample_idx}.png")
+    if generated_caption_1 == caption:
+        correct_generations_trained += 1
 
-    with open(f"temp/image_{sample_idx}_caption.txt", "w") as f:
+    if generated_caption_2 == caption:
+        correct_generations_base += 1
+
+    with open(f"temp/generations.txt", "a") as f:
         f.write(f"Actual Caption: {caption}\n")
-        f.write(f"Generated Caption: {generated_caption}\n")
+        f.write(f"Trained Model - Generated Caption: {generated_caption_1}\n")
+        f.write(f"Base Model    - Generated Caption: {generated_caption_2}\n\n")
 
-
-
-
-imgs = [
-    {
-        'url': 'https://datasets-server.huggingface.co/cached-assets/linxy/LaTeX_OCR/--/89aa6e447dd7afb4dec927af549df766539b6f9c/--/human_handwrite_print/train/0/image/image.jpg?Expires=1733310855&Signature=ILkAdXeXZ2K2ouUu8PjTfVi7BKSTQax1hNT04EYi2PrJmuewAM-JxAkC5VuPINekFCkbWqNf0V48FeVRylXuhHT1SQwfF7OXk~MwZ50hefzgr5E9HYqocIs5KgVNi8lAw6WgcQ~2MYKe9Rufy2lIFzchr0BfWqnbL1tmJXOlMjSKL78mb7vihoffoLyXcPpAQy2p0EjmZUvUlKR71wuQFtlT4lw5UOdPzk0oUNFvXt~E~42RC~5lhJMtmsYBm5KznMNTwoEiauHXnWu~QO14Z0ypgCOt1~mCDyILyKZCVhVcl6LHtstrk70-Id8djnEgQueQEvXbL~7zvoDgXEU2rw__&Key-Pair-Id=K3EI6M078Z3AC3',
-        'meta': 'z _ { 1 } = r _ { 1 } ( \cos \theta _ { 1 } + i \sin \theta _ { 1 } )'
-    },
-    {
-        'url': 'https://datasets-server.huggingface.co/cached-assets/linxy/LaTeX_OCR/--/89aa6e447dd7afb4dec927af549df766539b6f9c/--/human_handwrite_print/train/6/image/image.jpg?Expires=1733300239&Signature=wDz9TVpqHYeVq8tiM~xziv8k2-QxC5qIF1Ph4pJZpQV4xJfbMzA4Polzy7jMFXwkfJCET48cWTXLj9OE6t7fk2k-J9XIszVOBPfBCRn87WNCN3yEe0EhMIwLjMvg4JqFHel4LwkMLA6aMvg5pi9wgeZliZcA3smwGZecwZt5JLKNmo63v90nc695RRJPe1Tkko8IZuFp9WFR4oGQegdIlbno6I-iX-ZNXvdKrafq0-9KeQE~E2nOTwptJ5JEbZuI9E1a5bXEMufvw2v-87mLirEPptW2Zs1TOUojixQwSfnpCa6MgNuT3~92gs~s~ItTR2sc6UHm46HLAW5Ws7WcbQ__&Key-Pair-Id=K3EI6M078Z3AC3',
-        'meta': '\frac { \tan \alpha - \tan \beta } { 1 + \tan \alpha \tan \beta }'
-    }
-]
-
-
-# Generate captions for the daily cartoons
-for idx, img in enumerate(imgs):
-    # download the image
-    response = requests.get(img['url'])
-    img = Image.open(BytesIO(response.content))
-    img = img.convert('RGB')
-
-    # save the image
-    img.save(f"web_image_{idx}.png")
-
-    # process the image
-    image = image_processor(images = img, return_tensors="pt")
-    image = {key: val.squeeze(0) for key, val in image.items()}  # Remove batch dimension
-
-    # generate the caption
-    generated_caption = multimodal_model.generate(image, max_new_tokens=120)
-
-    # save the caption
-    with open(f"web_image_{idx}_caption.txt", "w") as f:
-        f.write(f"Meta: {img['meta']}\n")
-        f.write(f"Generated Caption: {generated_caption}\n")
-
-
+with open(f"temp/generations.txt", "a") as f:
+        f.write(f"--------------------------------------------------\n")
+        f.write(f"Trained model accuracy: {correct_generations_trained / test_samples}")
+        f.write(f"Base model accuracy: {correct_generations_base / test_samples}")
